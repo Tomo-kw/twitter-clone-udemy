f1a0bfd918f1c6279f927f977aaa4d2cb7c9a72c
Tomo-kw <mulberryfield412@yahoo.ne.jp>
2022-08-27 17:36:51 +0900

    material ui の導入

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,13 +1,17 @@
 import React from "react";
+import TwitterIcon from "@mui/icons-material/Twitter";
 
 function Sidebar() {
   return (
     <div className="sidebar">
       {/* ツイッターアイコン(material ui) */}
+      <TwitterIcon className="sidebar--twitterIcon" />
+
       {/* サイドバーオプション */}
       {/* サイドバーオプション */}
       {/* サイドバーオプション */}
       {/* サイドバーオプション */}
+
       {/* ツイートボタン */}
     </div>
   );
