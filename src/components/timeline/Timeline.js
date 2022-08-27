12efe4ed3561d313c2142d342dc944fe4a39a9e6
Tomo-kw <mulberryfield412@yahoo.ne.jp>
2022-08-27 23:12:15 +0900

    タイムラインのコンポーネント・CSS実装

---
+++ src/components/timeline/Timeline.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Post from "./Post";
+import "./Timeline.css";
+import TweetBox from "./TweetBox";
+
+function Timeline() {
+  return (
+    <div className="timeline">
+      {/* ヘッダー */}
+      <div className="timeline__header">
+        <h2>ホーム</h2>
+      </div>
+      {/* ツイートボックス */}
+      <TweetBox />
+
+      {/* 投稿要素（POST情報） */}
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </div>
+  );
+}
+
+export default Timeline;
