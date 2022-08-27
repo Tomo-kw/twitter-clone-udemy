fb42c98fd66c9f5507933a108eb230618015af41
Tomo-kw <mulberryfield412@yahoo.ne.jp>
2022-08-27 16:30:08 +0900

    sidebarコンポーネント作成

---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      {/* ツイッターアイコン(material ui) */}
+      {/* サイドバーオプション */}
+      {/* サイドバーオプション */}
+      {/* サイドバーオプション */}
+      {/* サイドバーオプション */}
+      {/* ツイートボタン */}
+    </div>
+  );
+}
+
+export default Sidebar;
