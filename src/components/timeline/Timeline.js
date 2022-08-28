c295c113866601a9061b45425ef237986fbd1a6a
Tomo-kw <mulberryfield412@yahoo.ne.jp>
2022-08-28 10:50:15 +0900

    useEffectを使用し、postsデータの取得タイミングを設定

--- src/components/timeline/Timeline.js
+++ src/components/timeline/Timeline.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Post from "./Post";
 import "./Timeline.css";
 import TweetBox from "./TweetBox";
@@ -8,13 +8,15 @@ import { collection, getDocs } from "firebase/firestore";
 function Timeline() {
   const [posts, setPosts] = useState([]);
 
-  const postDate = collection(db, "posts");
-  // DocsはPromiseオブジェクト
-  getDocs(postDate).then((querySnapshot) => {
-    // console.log(querySnapshot.docs.map((doc) => doc.data()));
-    setPosts(querySnapshot.docs.map((doc) => doc.data()));
-  });
-  console.log(posts);
+  useEffect(() => {
+    const postDate = collection(db, "posts");
+    // DocsはPromiseオブジェクト
+    getDocs(postDate).then((querySnapshot) => {
+      // console.log(querySnapshot.docs.map((doc) => doc.data()));
+      setPosts(querySnapshot.docs.map((doc) => doc.data()));
+    });
+    console.log("aaa");
+  }, []);
 
   return (
     <div className="timeline">
@@ -28,6 +30,7 @@ function Timeline() {
       {/* 投稿要素（POST情報） */}
       {posts.map((post) => (
         <Post
+          key={post.text}
           displayName={post.displayName}
           username={post.username}
           verified={post.verified}
